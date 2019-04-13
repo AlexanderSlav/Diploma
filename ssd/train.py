c7735a4ea28b6bdbfe3b9199e8d63e66cc0f448b
Iliya Slavutin <iliya.slavutin@gmail.com>
2019-04-13 15:26:31 +0300

    Update train.py

--- ssd/train.py
+++ ssd/train.py
@@ -196,7 +196,7 @@ def train():
             update_vis_plot(iteration, loss_l.data[0], loss_c.data[0],
                             iter_plot, epoch_plot, 'append')
 
-        if iteration != 0 and iteration % 10 == 0:
+        if iteration != 0 and iteration % 500 == 0:
             print('Saving state, iter:', iteration)
             torch.save(ssd_net.state_dict(), 'weights/mymodel.pth')
             cp_file = 'cp weights/mymodel.pth /content/drive/My\ Drive/thelatest.pth'
