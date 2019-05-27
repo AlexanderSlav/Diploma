010690e3302efffee1ea66d28eab64240d80e4f7
AlexanderSlav <alexander.slavutin@gmail.com>
2019-05-27 16:23:38 +0300

    Update voc0712.py

--- ssd/data/voc0712.py
+++ ssd/data/voc0712.py
@@ -103,7 +103,7 @@ class VOCDetection(data.Dataset):
         self.name = 'train_val' #'dataset_name
         
         self._annopath = osp.join('%s', 'Annotations', '%s.xml')
-        self._imgpath = osp.join('%s', 'JPEGImages', '%s.jpeg')
+        self._imgpath = osp.join('%s', 'JPEGImages', '%s.jpg')
         self.ids = list()
         for (year, name) in image_sets:
             rootpath = osp.join(self.root, 'VOC' + year)
