d8f52e526a6449ea3a5103abe0cb2e6ec0f22e1b
AlexanderSlav <alexander.slavutin@gmail.com>
2019-05-29 14:59:52 +0300

    Update voc0712.py

--- ssd/data/voc0712.py
+++ ssd/data/voc0712.py
@@ -17,12 +17,8 @@ if sys.version_info[0] == 2:
 else:
     import xml.etree.ElementTree as ET
 
-VOC_CLASSES = (  # always index 0
-    'aeroplane', 'bicycle', 'bird', 'boat',
-    'bottle', 'bus', 'car', 'cat', 'chair',
-    'cow', 'diningtable', 'dog', 'horse',
-    'motorbike', 'person', 'pottedplant',
-    'sheep', 'sofa', 'train', 'tvmonitor')
+VOC_CLASSES = [(  # always index 0
+   'player')]
 
 # note: if you used our download scripts, this should be right
 VOC_ROOT = osp.join(HOME, "VOCdevkit/")
